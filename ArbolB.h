46051892960f1d0d49f3d8fefa73d54d7a0c995c
Deivbid <davidaapariciobak@gmail.com>
2017-04-15 16:09:12 -0400

    Add files via upload

--- ArbolB.h
+++ ArbolB.h
@@ -25,7 +25,19 @@ class ArbolB
 		void recorridoPreorden(NodoA<T> *p, Lista<T> &Pre);//Recorre y guarda el Preorden en una lista 
 		void recorridoPosorden(NodoA<T> *p, Lista<T> &Pos);// Recorre y guarda el Posorden en una lista
 		void recorridoInorden(NodoA<T> *p, Lista<T> &Pos);//Recorre y guarda el Inorden
-		void recorridoNiveles(NodoA<T> *p, Lista<T> &Niv);
+		void recorridoNiveles(NodoA<T> *p, Lista<T> &Niv);//Recorre y guarda por niveles 
+		void recorridoLimitado(NodoA<T> *p, Lista<T> &Lim, int inicio, int fin); // Recorre y guarda dependiendo de los limites si existen
+		void _recorridoPosorden(NodoA<T> *p, Lista<T> &Pos);// Recorre y guarda el Posorden invertido en una lista
+		void piramide(NodoA<T> *p, bool &band, int &suma);//Devuelve un booleano diciendo si es piramido o no, piramide = la suma de los hijos es igual al padre
+		void frontera(NodoA<T> *p, Lista<T> &L);//Devuelve todos los nodos Hoja del arbol en la lista L
+		int talla(NodoA<T> *p); //Calcula el numero de nodos de la rama mas larga
+		void zurdo(NodoA<T> *p, bool &band, int &suma); //Determina si el arbol es Zurdo, unas reglas locas ahi 
+		void esBB(NodoA<T> *p, bool &band, int &min, int &max);//Determina si es Binario de busqueda , mas reglas locas ahi
+		void gordura(NodoA<T> *p, int &anchura); //Determina el nivel con mas nodos, es decir, la gordura del arbol
+		void esAVL(NodoA<T> *p, bool &band, int &min, int &max, int &peso); //Determina si el arbol es BB y esta balanceado
+		NodoA<T> * espejo(NodoA<T> *p);//Espejo pls
+		bool esCompleto(NodoA<T> *p);//Determina si el arbol es completo es decir que todos los nodos tienen o 0 o 2 hijos exactamente
+		
 
 	
 	public:
@@ -35,14 +47,26 @@ class ArbolB
 		~ArbolB();
 
 		//Retorno de data
-		bool esNulo();
-		int peso();
-		T obtRaiz();
-		ArbolB<T> hijoIzq();
-		ArbolB<T> hijoDer();
-		bool esHoja();
-		ArbolB<T> operator=(const ArbolB<T> &A);
-		int altura();
+		bool esPiramide(); //Determina si es piramide o no
+		bool esNulo(); // Detemrina si el arbol es nulo o no
+		bool esHoja(); //Determina si la raiz es hoja
+		bool esZurdo(); //Determina si el arbol es zurdo o no
+		bool esBB(); //Determina si el arbol es Binario de Busqueda
+		bool esAVL();//Determina si es Binario de Busqueda Balanceado
+		bool esCompleto();//Es completo si todos los nodos no terminales tienen exactamente 2 hijos
+		int peso(); //Retorna el numero de nodos del arbol
+		int altura();//el numero de arcos desde la raiz a la hoja mas lejana
+		int talla();//el numero de nodos de la rama mas larga
+		int gordura();//Retorna el numero de nodos de el nivel mas "gordo" jeje 
+		T obtRaiz();// Retorna el contenido de la raiz
+		ArbolB<T> hijoIzq();//Retorna el subarbol izquierdo es decir su Hijo izquierdo 
+		ArbolB<T> hijoDer();//Retorna el subarbol derecho es decir su Hijo Derecho
+		ArbolB<T> operator=(const ArbolB<T> &A); // Pls
+		ArbolB<T> espejo();//Retorna un Arbol espejo del pasado por parametro
+		Lista<T> frontera(); // Retorna lista con todos los nodos hoja del arbol 
+		
+		
+		
 
 		//Recorridos
 		Lista<T> Preorden();
@@ -50,6 +74,8 @@ class ArbolB
 		Lista<T> Inorden();
 		Lista<T> Niveles();
 		Lista<T> Niveles_limites(int limi, int limif);
+		Lista<T> Posorden_invertido();
+
 
 		//void's
 		void insertarHi(const ArbolB<T> A); //NO DEBE TENER HIJO IZQ ANTES DE INSERTAR
@@ -57,8 +83,8 @@ class ArbolB
 		void eliminarHi();
 		void eliminarHd();
 		void borrar();
-		void lecturaPrIn(Lista<T> L1, Lista<T> L2);// LECTURA DE LISTAS PREORDEN , INORDEN
-		void lecturaPosIn(Lista<T> L1, Lista<T> L2);// LECTURA DE LISTAS POSORDEN, INORDEN
+		void lecturaPrIn(Lista<T> L1, Lista<T> L2);// LECTURA DE LISTAS PREORDEN , INORDEN, NO DEBEN TENER ELEMENTOS REPETIDOS
+		void lecturaPosIn(Lista<T> L1, Lista<T> L2);// LECTURA DE LISTAS POSORDEN, INORDEN, NO DEBEN TENER ELEMENTOS REPETIDOS
 		void imprimir();
 
 		
@@ -247,7 +273,7 @@ NodoA<T> * ArbolB<T>::lecturaPrIn(Lista<T> &Pre, Lista<T> In, int posi, int posf
 	T aux;
 	NodoA<T> *nuevo;
 	
-	if(posi <= posf)
+	if(posi <= posf && !Pre.esVacia())
 	{
 		aux = Pre.consultar(1);
 		Pre.eliminar(1);
@@ -513,6 +539,485 @@ void ArbolB<T>::recorridoNiveles(NodoA<T> *p, Lista<T> &Niv)
 		}
 
 		Niv.insertar(aux -> obtInfo(), 1);
+	}
+}
+
+template <class T>
+Lista<T> ArbolB<T>::Niveles_limites(int posi, int posf)
+{
+	int alt;
+	Lista<T> L;
+
+	alt = this -> altura();
+
+	if(posi <= alt && posf <= alt && !this -> esNulo())
+	{
+		if(posi > posf)
+		{
+			this -> recorridoLimitado(this -> raiz, L, posf, posi);
+		}
+		else
+		{
+			this -> recorridoLimitado(this -> raiz, L, posi, posf);
+		}
+	}
+
+	L.invertir();
+
+	return L;
+}
+
+template <class T>
+void ArbolB<T>::recorridoLimitado(NodoA<T> *p, Lista<T> &Lim, int inicio, int fin)
+{
+	Cola< NodoA<T> * > C;
+	int i, aux, j;
+
+	i = 0;
+	aux = 1;
+
+	C.Encolar(p);
+	while(i < inicio)
+	{
+		for(j = 0 ; j < aux ; j++)
+		{
+			p = C.Obtfrente();
+
+			if(p -> obtHi() != NULL)
+			{
+				C.Encolar(p -> obtHi());
+			}
+
+			if(p -> obtHd() != NULL)
+			{
+				C.Encolar(p -> obtHd());
+			}
+
+			C.Desencolar();
+		}
+
+		aux = aux * 2;
+		i++;
+	}
+
+	i = fin - inicio ;
+	
+	while(i != 0)
+	{
+		for(j = 0 ; j < aux ; j++)
+		{
+			p = C.Obtfrente();
+			
+			Lim.insertar(p -> obtInfo(), 1);
+
+			if(p -> obtHi() != NULL)
+			{
+				C.Encolar(p -> obtHi());
+			}
+
+			if(p -> obtHd() != NULL)
+			{
+				C.Encolar(p -> obtHd());
+			}
+
+			C.Desencolar();
+		}
+
+		aux = aux * 2;
+		i--;
+	}
+
+	while(!C.esVacia())
+	{
+		p = C.Obtfrente();
+		Lim.insertar(p -> obtInfo(), 1);
+		C.Desencolar();
+	}
+
+}
+
+template <class T>
+Lista<T> ArbolB<T>::Posorden_invertido()
+{
+	Lista<T> L;
+
+	if(!this -> esNulo())
+	{
+		this -> _recorridoPosorden(this -> raiz, L);
+	}
+
+	L.invertir();
+
+	return L;
+}
+
+template < class T>
+void ArbolB<T>::_recorridoPosorden(NodoA<T> *p, Lista<T> &Pos)
+{
+	if(p != NULL)
+	{
+		this -> _recorridoPosorden(p -> obtHd(), Pos);
+		this -> _recorridoPosorden(p -> obtHi(), Pos);
+		Pos.insertar(p -> obtInfo(), 1);
+	}
+}
+
+template <class T>
+bool ArbolB<T>::esPiramide()
+{
+	bool band;
+	int suma;
+
+	this -> piramide(this -> raiz, band, suma);
+
+	return band;
+}
+
+template <class T>
+void ArbolB<T>::piramide(NodoA<T> *p, bool &band, int &suma)
+{
+	int pi,pd;
+
+	if(p == NULL)
+	{
+		band = true;
+		suma = 0;
+	}
+	else
+	{
+		if(p -> obtHi() == NULL && p -> obtHd() == NULL)
+		{
+			band = true;
+			suma = p -> obtInfo();
+		}
+		else
+		{
+			this -> piramide(p -> obtHi(), band, pi);
+			if(band)
+			{
+				this -> piramide(p -> obtHd(), band, pd);
+			}
+
+			band = (pi + pd) == p -> obtInfo();
+			suma = pi + pd;
+		}
+	}
+}
+
+template <class T>
+Lista<T> ArbolB<T>::frontera()
+{
+	Lista<T> L;
+	
+	if(!this -> esNulo())
+	{
+		this -> frontera(this -> raiz, L);
+	}
+
+	L.invertir();
+
+	return L;
+}
+
+template <class T>
+void ArbolB<T>::frontera(NodoA<T> *p, Lista<T> &L)
+{
+	if(p != NULL)
+	{
+		if(p -> obtHi() == NULL && p -> obtHd() == NULL)
+		{
+			L.insertar(p -> obtInfo(),1);
+		}
+		else
+		{
+			frontera(p -> obtHi(), L);
+			frontera(p -> obtHd(), L);
+		}
+	}
+}
+
+template <class T>
+int ArbolB<T>::talla()
+{
+	return this -> talla(this -> raiz);
+}
+
+template <class T>
+int ArbolB<T>::talla(NodoA<T> *p)
+{
+	int pi, pd;
+	
+	if(p != NULL)
+	{
+		pi = this -> talla(p -> obtHi()) + 1;
+		pd = this -> talla(p -> obtHd()) + 1;
+
+		if(pi > pd)
+		{
+			return pi;
+		}
+		else
+		{
+			return pd;
+		}
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+template <class T>
+bool ArbolB<T>::esZurdo()
+{
+	bool band;
+	int suma;
+
+	this -> zurdo(this -> raiz, band, suma);
+
+	return band;
+}
+
+template <class T>
+void ArbolB<T>::zurdo(NodoA<T> *p, bool &band, int &suma)
+{
+	bool bi, bd;
+	int si, sd;
+
+	if(p == NULL)
+	{
+		band = true;
+		suma = 0;
+	}
+	else
+	{
+		if( p -> obtHi() == NULL && p -> obtHd() == NULL)
+		{
+			band = true;
+			suma = 1;
+		}
+		else
+		{
+			this -> zurdo(p -> obtHi(), bi, si);
+			if(bi)
+			{
+				this -> zurdo(p -> obtHd(), bd, sd);
+			}
+
+			band = bi && bd && si > sd;
+			suma = si + sd + 1;
+		}
+	}
+}
+
+template <class T>
+bool ArbolB<T>::esBB()
+{
+	bool band;
+	int min, max;
+
+	this -> esBB(this -> raiz, band, min, max);
+
+
+	return band;
+}
+
+template <class T>
+void ArbolB<T>::esBB(NodoA<T> *p, bool &band, int &min, int &max)
+{
+	int mini, mind, maxi, maxd;
+	bool bi, bd;
+
+	if(p == NULL)
+	{
+		band = true;
+		min = 9999;
+		max = -9999;
+	}
+	else
+	{
+		if(p -> obtHi() == NULL && p -> obtHd() == NULL)
+		{
+			band = true;
+			min = p -> obtInfo();
+			max = p -> obtInfo();
+		}
+		else
+		{
+			this -> esBB(p -> obtHi(), bi, mini, maxi);
+			if(bi)
+			{
+				this -> esBB(p -> obtHd(), bd, mind, maxd);
+			}
+
+			band = bi && bd && p -> obtInfo() > maxi && p -> obtInfo() < mind;
+			min = mini;
+			max = maxd;
+		}
+	}
+}
+
+template <class T>
+int ArbolB<T>::gordura()
+{
+	int anchura;
+
+	this -> gordura(this -> raiz, anchura);
+
+	return anchura;
+}
+
+template <class T>
+void ArbolB<T>::gordura(NodoA<T> *p, int &anchura)
+{
+	Cola< NodoA<T> * > C;
+	int i, pot, acum, alt;
+
+	C.Encolar(p);
+	pot = 1;
+	acum = 0;
+	anchura = 1;
+	alt = this -> altura();
+	while(alt != 0)
+	{
+		for(i = 0 ; i < pot ; i++)
+		{
+			p = C.Obtfrente();
+
+			if(p -> obtHi() != NULL)
+			{
+				C.Encolar(p -> obtHi());
+				acum++;
+			}
+
+			if(p -> obtHd() != NULL)
+			{
+				C.Encolar(p -> obtHd());
+				acum++;
+			}
+			
+			C.Desencolar();
+			
+		}
+
+		if(acum > anchura)
+		{
+			anchura = acum;
+		}
+
+		acum = 0;
+		pot = pot * 2;
+		alt--;
+
+	}
+}
+
+template <class T>
+bool ArbolB<T>::esAVL()
+{
+	bool band;
+	int min, max, peso;
+
+	this -> esAVL(this -> raiz, band, min, max, peso);
+
+	return band;
+}
+
+template <class T>
+void ArbolB<T>::esAVL(NodoA<T> *p, bool &band, int &min, int &max, int &peso)
+{
+	bool bi,bd;
+	int mini, mind, maxi, maxd, pi, pd;
+
+	if(p == NULL)
+	{
+		band = true;
+		min = 9999;
+		max = -9999;
+		peso = 0;
+	}
+	else
+	{
+		if(p -> obtHi() == NULL && p -> obtHd() == NULL)
+		{
+			band = true;
+			min = p -> obtInfo();
+			max = p -> obtInfo();
+			peso = 1;
+		}
+		else
+		{
+			this -> esAVL(p -> obtHi(), bi, mini, maxi, pi);
+			if(bi)
+			{
+				this -> esAVL(p -> obtHd(), bd, mind, maxd, pd);
+			}
+	
+			band = bi && bd && p -> obtInfo() > maxi && p -> obtInfo() < mind && abs(pi - pd) <= 1;
+			min = mini;
+			max = maxd;
+			peso = pi + pd;
+		}
+	}
+}
+
+template <class T>
+ArbolB<T> ArbolB<T>::espejo()
+{
+	ArbolB<T> nuevo;
+
+	nuevo.raiz = this -> espejo(this -> raiz);
+	nuevo.n = this -> n;
+
+	return nuevo;
+}
+
+template <class T>
+NodoA<T> * ArbolB<T>::espejo(NodoA<T> *p)
+{
+	NodoA<T> *nuevo;
+
+	if(p != NULL)
+	{
+		nuevo = new NodoA<T> (p -> obtInfo(), espejo(p -> obtHd()), espejo(p -> obtHi()));
+		return nuevo;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+template <class T>
+bool ArbolB<T>::esCompleto()
+{
+	return this -> esCompleto(this -> raiz);
+}
+
+template <class T>
+bool ArbolB<T>::esCompleto(NodoA<T> *p)
+{
+	if(p == NULL)
+	{
+		return true;
+	}
+	else
+	{
+		if(p -> obtHi() == NULL && p -> obtHd() == NULL)
+		{
+			return true;
+		}
+		else
+		{
+			if(p -> obtHi() != NULL && p -> obtHd() != NULL)
+			{
+				return this -> esCompleto(p -> obtHi()) && this -> esCompleto(p -> obtHd());
+			}
+			else
+			{
+				return false;
+			}
+		}
 	}
 }
 
